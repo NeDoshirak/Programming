95113179c1a353c82094cde257695797735633d0
NeDoshirak <ujhjvjn@yandex.ru>
2024-02-16 01:05:21 +0700

    #

---
+++ Programming/Model/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public enum Genre
+    {
+        Сomedy,
+        Drama,
+        Thriller,
+        Action,
+        Horror,
+        Blockbuster
+    }
+
+}
