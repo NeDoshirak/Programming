0f21360a865e3758c87c4c058485447c6faf4c81
NeDoshirak <ujhjvjn@yandex.ru>
2025-02-26 00:05:30 +0700

    Первый коммит в этой ветке. Сверстал интерфейс, написал классы Contact и MainVM, начал работать над сериализацией.

---
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using View.Model;
+
+namespace View.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private Contact _contact = new Contact();
+
+        public string Name 
+        { 
+            get 
+            { 
+                return _contact.Name;
+            }
+            set 
+            { 
+                _contact.Name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _contact.PhoneNumber;
+            }
+            set
+            {
+                _contact.PhoneNumber = value;
+                NotifyPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _contact.Email;
+            }
+            set
+            {
+                _contact.Email = value;
+                NotifyPropertyChanged(nameof(Email));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
