e8a9ecaa2f998f96451f275dacce08024065134a
NeDoshirak <ujhjvjn@yandex.ru>
2025-03-10 01:04:46 +0700

    Рабоча сборка для первой рабораторной. Создан класс для сериализации данных, произведенна привязка MainWindow к MainVM

--- src/Contacts/View/ViewModel/MainVM.cs
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -1,26 +1,32 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using View.Model;
+using View.Model.Services;
 
 namespace View.ViewModel
 {
     public class MainVM : INotifyPropertyChanged
     {
         private Contact _contact = new Contact();
+        private static ContactSerializer _contactSerializer = new ContactSerializer();
+        public ICommand SaveCommand { get; }
+        public ICommand LoadCommand { get; }
 
-        public string Name 
-        { 
-            get 
-            { 
+        public string Name
+        {
+            get
+            {
                 return _contact.Name;
             }
-            set 
-            { 
+            set
+            {
                 _contact.Name = value;
                 NotifyPropertyChanged(nameof(Name));
             }
@@ -57,6 +63,21 @@ namespace View.ViewModel
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainVM()
+        {
+            _contact = new Contact();
+            _contactSerializer = new ContactSerializer();
+
+            SaveCommand = new RelayCommand(() => _contactSerializer.SaveContact(_contact));
+            LoadCommand = new RelayCommand(() =>
+                                            {
+                                                Contact contact = _contactSerializer.LoadContact();
+                                                Name = contact.Name;
+                                                PhoneNumber = contact.PhoneNumber;
+                                                Email = contact.Email;
+                                            });
         }
     }
 }
---
+++ src/Contacts/View/ViewModel/SaveCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using View.Model.Services;
+using View.Model;
+
+namespace View.ViewModel
+{
+    public class SaveCommand : ICommand
+    {
+        private ContactSerializer _serializer;
+        private Contact _contact;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            _serializer.SaveContact(_contact);
+        }
+
+        public SaveCommand(ContactSerializer serializer, Contact contact)
+        {
+            _serializer = serializer;
+            _contact = contact;
+        }
+    }
+}
